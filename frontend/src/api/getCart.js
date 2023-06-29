562c49a59642ef190c680d637a643f87e7514ae2
saderyan98 <saderyan98@hotmail.co.uk>
2023-06-29 11:15:59 +0100

    getcart and small changes

---
+++ frontend/src/api/getCart.js
@@ -0,0 +1,13 @@
+const API_URL = `http://localhost:4000`
+
+export const getCart = async () => {
+    const response = await fetch(`${API_URL}/products/items/`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    const json = await response.json()
+    return json
+
+}
