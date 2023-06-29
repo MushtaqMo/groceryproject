0046d655be80b29a7794fbf4370075d5108b3fd0
saderyan98 <saderyan98@hotmail.co.uk>
2023-06-29 05:41:21 +0100

    Update server.js

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const itemsController = require('./controllers/items');
+
+mongoose.connect('', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+
+const cartSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+      },
+    
+      price: {
+        type: Number,
+        required: true
+      },
+    
+      category: {
+        type: String,
+        required: true
+      },
+    
+      numberSelected: {
+        type: Number,
+        required: true
+      }
+    });
+
+    const Cart = mongoose.model('Cart', cartSchema)
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+
+    app.get('/', function(req,res)
+    {
+        res.sendFile(__dirname + '/frontend/index.html');
+    })
+
+    
+    app.get('/items', itemsController.getProducts);
+
+
+    app.post('/cart', itemsController.createProduct);
+
+
+    app.listen(3000, function() {
+        console.log("Server running on 3000")
+    })
