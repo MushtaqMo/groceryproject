124e5d074d463647d6283a61ff7ea6bd99f158e5
Oliver33Holland <oliverholland33@gmail.com>
2023-06-29 11:17:57 +0100

    navbar
    navbar

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+const Navbar = () => {
+    return (
+        <div>
+            <header>
+                <div>
+                    <Link to='../Home'>
+
+                    </Link>
+                    <Link to='../Basket'>
+
+                    </Link>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
