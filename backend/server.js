81cf5920da775fea6349064f087b82f0cd806a1e
saderyan98 <saderyan98@hotmail.co.uk>
2023-06-29 12:47:56 +0100

    server changes

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const itemsController = require('./controllers/items');
+const itemRoutes = require('./routes/items')
+const cors = require('cors')
 
-mongoose.connect('', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true})
-
-const cartSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-      },
-    
-      price: {
-        type: Number,
-        required: true
-      },
-    
-      category: {
-        type: String,
-        required: true
-      },
-    
-      numberSelected: {
-        type: Number,
-        required: true
-      }
-    });
-
-    const Cart = mongoose.model('Cart', cartSchema)
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-
-    app.get('/', function(req,res)
-    {
-        res.sendFile(__dirname + '/frontend/index.html');
-    })
-
-    
-    app.get('/items', itemsController.getProducts);
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('.models/cart.js', itemRoutes)
 
 
-    app.post('/cart', itemsController.createProduct);
+mongoose.connect(process.env.MONGODB_URL)
+// LISTEN ON PORT 4000
+// frontend is running on port 3000
+.then(() => {
 
 
     app.listen(4000, function() {
         console.log("Server running on 4000")
     })
+  })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,17 @@ const App = () => {
     return (
         <div>
             <h1>hello world</h1>
-
+        <Navbar />
+        <Routes>
+            <Route
+                path='/'
+                element={<Home />}
+            />
+            <Route 
+                path='/addToCart'
+                element={<addToCard/>}
+            />
+        </Routes>
 
         </div>
 
