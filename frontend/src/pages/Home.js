187b3c8ebffe479241168a90de62a9c725628d4a
Emma Holden <emmaholden01204@gmail.com>
2023-07-04 14:31:35 +0100

    navbar fixed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -36,7 +36,6 @@ const Home = () => {
             <Link to = '/fruitAndVeg'><button>Fruit and Vegetables</button></Link>
             <Link to = '/meatAndFish'><button>Meat and Fish</button></Link>
             <Link to = '/frozenFood'><button>Frozen Food</button></Link>
-            <Link to = '/cart'><button>Cart</button></Link> 
             <Search/> 
             
 
