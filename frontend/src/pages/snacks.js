047b6050dc8c533dc8b0679e0364f2542dfb18d8
Emma Holden <emmaholden01204@gmail.com>
2023-07-05 10:46:54 +0100

    added category pages css

--- frontend/src/pages/snacks.js
+++ frontend/src/pages/snacks.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import stock from '../availableStock'
 import { addToCart } from "../api/addToCart";
+import "./categories.css"
 
 const addHandler = async (item) => {
     let product = {
@@ -17,13 +18,15 @@ const ShowSnacks = () => {
     return (
         <div>
             <h1>Snacks</h1>
+            <div className = "categoryBox">
             {stock.filter(item => item.category === "snacks").map(snackStock =>
-            <div>
+            <div className = "categoryProduct">
                 <p>{snackStock.name}</p>
                 <p>£{snackStock.price.toFixed(2)}</p>
-                <button onClick = {() => addHandler(snackStock)}>Add To Basket</button>
+                <button className = "categoryButton" onClick = {() => addHandler(snackStock)}>Add To Basket</button>
             </div>
             )}
+            </div>
         </div>
     )
 }
